03e02096207b5f40dbbcf3a6f41b2e7fe07d67cc
JEEVAN R <121960177+jeevanrajan03@users.noreply.github.com>
2024-02-14 20:49:04 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello ImmanRaj Welcome to ReactNative Project</Text>
+  const [goalText, setGoalText] = useState("")
+  const [goals, setGoals] = useState([])
+  function goalImportHandler(text) {
+    setGoalText(text)
+  }
 
+  function addGoalHandler() {
+    setGoals((currentGoals) => [...currentGoals, goalText])
+  }
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput onChangeText={goalImportHandler} placeholder='Type your Goal' style={styles.textInput} />
+        <Button onPress={addGoalHandler} title='Add Goal' />
+      </View>
+
+      <View style={styles.goalsContainer}>
+        {goals.map((goal, i) => <Text style={styles.goalItem} key={i}>{goal}</Text>)}
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    paddingTop: 50,
+    paddingHorizontal: 16,
     flex: 1,
-    backgroundColor: '#ffa',
+  },
+
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+    borderBottomWidth: 1,
+    paddingBottom: 24,
+    borderBottomColor: '#cccccc',
+    flex: 1,
+  },
+
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8,
+  },
+
+  goalsContainer: {
+    flex: 4,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+    color: 'white',
   },
 });
